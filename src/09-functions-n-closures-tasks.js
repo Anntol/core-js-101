730edc379f016fb2aaa9b67dc33dbefa8cbb996e
Anna Tolstova <anna_tolstova@hotmail.com>
2020-12-15 00:14:44 +0200

    feat: part of closures tasks

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,10 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return function compose(...args) {
+    return f(g(...args));
+  };
 }
 
 
@@ -44,8 +46,10 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function power(x) {
+    return x ** exponent;
+  };
 }
 
 
@@ -62,8 +66,10 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return function polynom(x) {
+    return [...args].reverse().reduce((acc, item, index) => acc + item * (x ** index), 0);
+  };
 }
 
 
@@ -81,8 +87,14 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cachedValue;
+  return function cachedFn(...args) {
+    if (!cachedValue) {
+      cachedValue = func(...args);
+    }
+    return cachedValue;
+  };
 }
 
 
